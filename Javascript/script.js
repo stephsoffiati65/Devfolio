82db04cbb935ef22a9a3532eb318ca3675ba9377
stephsoffiati65 <stephanesoffiati65@gmail.com>
2022-03-08 14:45:19 +0100

    tentative menu

--- Javascript/script.js
+++ Javascript/script.js
@@ -2,79 +2,79 @@
 // Animation burger
 // ==================================================
 
-let menu = document.querySelector(".menu")
-let ham = document.querySelector(".ham")
-let xIcon = document.querySelector(".xIcon")
-let burgerIcon = document.querySelector(".burgerIcon")
-let a1 = document.querySelector("#a1")
-let a2 = document.querySelector("#a2")
-let a3 = document.querySelector("#a3")
-let a4 = document.querySelector("#a4")
-let a5 = document.querySelector("#a5")
-let a6 = document.querySelector("#a6")
+// let menu = document.querySelector(".menu")
+// let ham = document.querySelector(".ham")
+// let xIcon = document.querySelector(".xIcon")
+// let burgerIcon = document.querySelector(".burgerIcon")
+// let a1 = document.querySelector("#a1")
+// let a2 = document.querySelector("#a2")
+// let a3 = document.querySelector("#a3")
+// let a4 = document.querySelector("#a4")
+// let a5 = document.querySelector("#a5")
+// let a6 = document.querySelector("#a6")
 
 
-ham.addEventListener("click", toggleMenu)
+// ham.addEventListener("click", toggleMenu)
 
-function toggleMenu() {
-  if (menu.classList.contains("showMenu")) {
-    menu.classList.remove("showMenu");
-    xIcon.style.display = "none";
-    burgerIcon.style.display = "block";
-  } else {
-    menu.classList.add("showMenu");
-    xIcon.style.display = "block";
-    burgerIcon.style.display = "none";
-  }
-}
+// function toggleMenu() {
+//   if (menu.classList.contains("showMenu")) {
+//     menu.classList.remove("showMenu");
+//     xIcon.style.display = "none";
+//     burgerIcon.style.display = "block";
+//   } else {
+//     menu.classList.add("showMenu");
+//     xIcon.style.display = "block";
+//     burgerIcon.style.display = "none";
+//   }
+// }
 
-a1.addEventListener("click", toggleLink)
+// a1.addEventListener("click", toggleLink)
 
-function toggleLink() {
-  menu.classList.remove("showMenu");
-  xIcon.style.display = "none";
-  burgerIcon.style.display = "block";
-}
+// function toggleLink() {
+//   menu.classList.remove("showMenu");
+//   xIcon.style.display = "none";
+//   burgerIcon.style.display = "block";
+// }
 
-a2.addEventListener("click", toggleLink)
+// a2.addEventListener("click", toggleLink)
 
-function toggleLink() {
-  menu.classList.remove("showMenu");
-  xIcon.style.display = "none";
-  burgerIcon.style.display = "block";
-}
+// function toggleLink() {
+//   menu.classList.remove("showMenu");
+//   xIcon.style.display = "none";
+//   burgerIcon.style.display = "block";
+// }
 
-a3.addEventListener("click", toggleLink)
+// a3.addEventListener("click", toggleLink)
 
-function toggleLink() {
-  menu.classList.remove("showMenu");
-  xIcon.style.display = "none";
-  burgerIcon.style.display = "block";
-}
+// function toggleLink() {
+//   menu.classList.remove("showMenu");
+//   xIcon.style.display = "none";
+//   burgerIcon.style.display = "block";
+// }
 
-a4.addEventListener("click", toggleLink)
+// a4.addEventListener("click", toggleLink)
 
-function toggleLink() {
-  menu.classList.remove("showMenu");
-  xIcon.style.display = "none";
-  burgerIcon.style.display = "block";
-}
+// function toggleLink() {
+//   menu.classList.remove("showMenu");
+//   xIcon.style.display = "none";
+//   burgerIcon.style.display = "block";
+// }
 
-a5.addEventListener("click", toggleLink)
+// a5.addEventListener("click", toggleLink)
 
-function toggleLink() {
-  menu.classList.remove("showMenu");
-  xIcon.style.display = "none";
-  burgerIcon.style.display = "block";
-}
+// function toggleLink() {
+//   menu.classList.remove("showMenu");
+//   xIcon.style.display = "none";
+//   burgerIcon.style.display = "block";
+// }
 
-a6.addEventListener("click", toggleLink)
+// a6.addEventListener("click", toggleLink)
 
-function toggleLink() {
-  menu.classList.remove("showMenu");
-  xIcon.style.display = "none";
-  burgerIcon.style.display = "block";
-}
+// function toggleLink() {
+//   menu.classList.remove("showMenu");
+//   xIcon.style.display = "none";
+//   burgerIcon.style.display = "block";
+// }
 
 
 // Animation SVG
